904209969b309e1d7669642495b11f7d483a8b79
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-21 07:56:35 +0200

    Login component created.

---
+++ src/components/Login.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = () => {
+
+  }
+
+  return (
+
+    <div>
+      <h1>Login</h1>
+      <input type="text" name="username" id="username" value={username} onChange={handleUsername} />
+      <input type="password" name="password" id="password" value={password} onChange={handlePassword} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
